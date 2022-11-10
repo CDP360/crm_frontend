bc4923fed31927bb167f59648286223789c6f55d
pavbha <pavithrabharathi641998@gmail.com>
2022-11-10 11:28:17 +0530

    webites changes

--- src/Pages/Sites/Sites.js
+++ src/Pages/Sites/Sites.js
@@ -1,90 +1,94 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
+import { Outlet, useNavigate } from "react-router-dom";
+
+// import Container from "react-bootstrap/Container";
 import Dropdown from "react-bootstrap/Dropdown";
-import Search from "../../assests/search.png";
-import Plus from "../../assests/plus.png";
-import Card from "react-bootstrap/Card";
-import Funnel from "../../assests/funnel.png";
-import Spanner from "../../assests/spanner.png";
-import PlayButton from "../../assests/play-button.png";
-import Folder from "../../assests/folder.png";
-import "./styles/sites.scss";
+// import Search from "../../assests/search.png";
+// import Plus from "../../assests/plus.png";
+// import Card from "react-bootstrap/Card";
+// import Funnel from "../../assests/funnel.png";
+// import Spanner from "../../assests/spanner.png";
+// import PlayButton from "../../assests/play-button.png";
+// import Folder from "../../assests/folder.png";
+// import "./styles/sites.scss";
 
 const Sites = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="main-container">
       <div className="sites-header-container">
-      <div className="sites-header">
-        <p className="sites-heading">Sites</p>
-        <p>Funnels</p>
-        <p>Websites</p>
-        <p>Blog</p>
-        <Dropdown>
-          <Dropdown.Toggle
-            variant="light"
-            id="dropdown-basic"
-            className="sites-dropdown"
-          >
-            WordPress
-          </Dropdown.Toggle>
+        <div className="sites-header">
+          <p className="sites-heading">Sites</p>
+          <p onClick={() => navigate("")}>Funnels</p>
+          <p onClick={() => navigate("/sites/websites")}>Websites</p>
+          <p>Blog</p>
+          <Dropdown>
+            <Dropdown.Toggle
+              variant="light"
+              id="dropdown-basic"
+              className="sites-dropdown"
+            >
+              WordPress
+            </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Dashboard</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        <Dropdown>
-          <Dropdown.Toggle
-            variant="light"
-            id="dropdown-basic"
-            className="sites-dropdown"
-          >
-            Membership
-          </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Dashboard</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Dropdown>
+            <Dropdown.Toggle
+              variant="light"
+              id="dropdown-basic"
+              className="sites-dropdown"
+            >
+              Membership
+            </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Dashboard</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Products</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Offers</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Analytics</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Settings</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        <Dropdown>
-          <Dropdown.Toggle
-            variant="light"
-            id="dropdown-basic"
-            className="sites-dropdown"
-          >
-            Forms
-          </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Dashboard</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Products</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Offers</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Analytics</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Settings</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Dropdown>
+            <Dropdown.Toggle
+              variant="light"
+              id="dropdown-basic"
+              className="sites-dropdown"
+            >
+              Forms
+            </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Builders</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Analyze</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Submissions</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        <Dropdown>
-          <Dropdown.Toggle
-            variant="light"
-            id="dropdown-basic"
-            className="sites-dropdown"
-          >
-            Surveys
-          </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Builders</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Analyze</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Submissions</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Dropdown>
+            <Dropdown.Toggle
+              variant="light"
+              id="dropdown-basic"
+              className="sites-dropdown"
+            >
+              Surveys
+            </Dropdown.Toggle>
 
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Builders</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Analyze</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Submissions</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        <p>Chat Widget</p>
-        <p>Media</p>
-        <p>URL Redirection</p>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Builders</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Analyze</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Submissions</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <p>Chat Widget</p>
+          <p>Media</p>
+          <p>URL Redirection</p>
+        </div>
       </div>
-      </div>
-      <div className="sites-body">
+      {/* <div className="sites-body">
         <div>
           <p>Funnels</p>
         </div>
@@ -255,7 +259,8 @@ const Sites = () => {
             </div>
           </Container>
         </div>
-      </div>
+      </div> */}
+      <Outlet />
     </div>
   );
 };
